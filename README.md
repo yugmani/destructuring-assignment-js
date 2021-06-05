16b111eaae83ece6fa2d6713a3425214094deae7
yugmani <yugmani@yahoo.com>
2021-06-04 23:31:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# destructuring-assignment-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/destructuring-assignment-js)
---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Destructuring Assignment in JavaScript
+
+// 1 . General implementation of the extraction of the array:
+
+const ["alpha", "beta", "gamma", "delta"]
+
