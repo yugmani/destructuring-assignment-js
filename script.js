f786a77a671a40b58ca1e80a02a923e1854d22ce
yugmani <yugmani@yahoo.com>
2021-06-04 23:38:50 -0700

    general extraction of the array

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-
 // Destructuring Assignment in JavaScript
 
 // 1 . General implementation of the extraction of the array:
 
-const ["alpha", "beta", "gamma", "delta"]
+const names = ['alpha', 'beta', 'gamma', 'delta'];
+function extractingArray(arr, num) {
+  return (yourName = arr[num]);
+}
+
+console.log(extractingArray(names, 0));
+//Expected "alpha";
+console.log(extractingArray(names, 1));
+//Expected "beta";
+console.log(extractingArray(names, 2));
+//Expected "gamma";
+console.log(extractingArray(names, 3));
+//Expected "delta";
+console.log(extractingArray(names, 7));
+//Expected undefined
 
